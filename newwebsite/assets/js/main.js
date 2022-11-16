c15853aedc4bfb9965d762f82df61b9fc4564709
unbeatenahad22@gmail.com <unbeatenahad22@gmail.com>
2022-11-17 00:28:24 +0500

    document changes 16-11

--- newwebsite/assets/js/main.js
+++ newwebsite/assets/js/main.js
@@ -1,6 +1,29 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction();
+  scrollFunction();
+};
 
+// Get the button:
+const mybutton = document.querySelector('.scrollTopBtn');
+
+function scrollFunction() {
+  console.log('working 1');
+  console.log(mybutton);
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    console.log('working 2');
+  } else {
+    mybutton.style.display = "none";
+    console.log('working 3');
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 // Get the header
 let header = document.getElementById("myHeader");
 
